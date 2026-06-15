JKamsker/SafeIR

Bypass full results for no-audit prepared values
Installed kernels now use an internal prepared-value result for compiled entrypoints that have no binding references and suppress successful audit. This avoids constructing the public SandboxExecutionResult, resource snapshot, and empty audit result on successful ShouldHandle-style runs while failures and audited entrypoints continue through the existing full-result path.

The benchmark history records the focused --probe-prepared-values result: the full-result path measured 527.7 ms and 276,043,008 B for 200k compiled no-audit misses with the branch temporarily disabled, while enabled samples measured 388.3-428.9 ms and 227,155,008-230,065,792 B. A workflow sanity sample is included because the end-to-end dispatch probe remains noisy and still far from native.

benchmarks/SafeIR.Benchmarks/Examples/PreparedValueProbe.cs
@@ -1,0 +1,70 @@
+namespace SafeIR.Benchmarks.Examples;
+
+using System.Diagnostics;
+using SafeIR;
+using SafeIR.Plugins;
+
+internal static class PreparedValueProbe
+{
+    public static async Task RunAsync()
+    {
+        const int warmup = 2_000;
+        const int iterations = 200_000;
+        var e = new ExampleWorkflowProbe.DamageEvent("ice", 120, "player-1");
+        var summary = await MeasureShouldHandleMissAsync(warmup, iterations, e);
+
+        Console.WriteLine("case                         iterations   elapsed       allocated      handled");
+        Console.WriteLine(
+            $"compiled no-audit miss {iterations,14:N0} {summary.Milliseconds,8:N1} ms {summary.AllocatedBytes,13:N0} B {summary.Handled,10:N0}");
+    }
+
+    private static async Task<RunSummary> MeasureShouldHandleMissAsync(
+        int warmup,
+        int iterations,
+        ExampleWorkflowProbe.DamageEvent e)
+    {
+        using var server = PluginServer.Create(
+            new InMemoryPluginMessageSink(),
+            defaultPolicy: MessageWritePolicy(),
+            executionMode: ExecutionMode.Compiled);
+        var kernel = await server.InstallJsonAsync(ExampleWorkflowProbe.FireDamagePackageJson());
+        var adapter = ExampleWorkflowProbe.DamageEventAdapter.Instance;
+
+        for (var i = 0; i < warmup; i++)
+        {
+            _ = await kernel.ShouldHandleAsync(adapter, e);
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        var allocatedBefore = GC.GetAllocatedBytesForCurrentThread();
+        var sw = Stopwatch.StartNew();
+        var handled = 0;
+        for (var i = 0; i < iterations; i++)
+        {
+            if (await kernel.ShouldHandleAsync(adapter, e))
+            {
+                handled++;
+            }
+        }
+
+        sw.Stop();
+        var allocatedBytes = GC.GetAllocatedBytesForCurrentThread() - allocatedBefore;
+        return new RunSummary(sw.Elapsed.TotalMilliseconds, allocatedBytes, handled);
+    }
+
+    private static SandboxPolicy MessageWritePolicy()
+        => SandboxPolicyBuilder.Create()
+            .GrantLogging()
+            .GrantHostMessageWrite()
+            .WithFuel(long.MaxValue)
+            .WithMaxHostCalls(int.MaxValue)
+            .WithMaxLoopIterations(long.MaxValue)
+            .WithMaxTotalStringBytes(long.MaxValue)
+            .WithWallTime(TimeSpan.FromMinutes(5))
+            .Build();
+
+    private readonly record struct RunSummary(double Milliseconds, long AllocatedBytes, int Handled);
+}

src/SafeIR.Hosting/Execution/Prepared/CompiledNoAuditValueRunner.cs
@@ -1,0 +1,58 @@
+namespace SafeIR.Hosting;
+
+using SafeIR;
+
+internal static class CompiledNoAuditValueRunner
+{
+    public static PreparedExecutionResult Execute(
+        CompiledExecutable executable,
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        IReadOnlySet<string> allowedBindings,
+        CancellationToken cancellationToken)
+    {
+        var artifact = executable.Artifact;
+        var budget = new ResourceMeter(plan.Budget);
+        var context = new SandboxContext(
+            SandboxRunId.Suppressed,
+            plan.Policy,
+            budget,
+            plan.Bindings,
+            NoopAuditSink.Instance,
+            cancellationToken,
+            allowedBindings,
+            plan.ModuleHash,
+            plan.PolicyHash);
+
+        try
+        {
+            budget.CheckDeadline();
+            context.ChargeValue(input);
+            var value = artifact.Entrypoint(context, input);
+            CompiledExecutionRunner.EnsureReturnType(plan, entrypoint, value);
+            return PreparedExecutionResult.FromNoAuditSuccess(
+                value,
+                ExecutionMode.Compiled,
+                artifact.ArtifactHash);
+        }
+        catch (OperationCanceledException)
+        {
+            var error = new SandboxError(SandboxErrorCode.Cancelled, "execution cancelled");
+            return PreparedExecutionResult.FromResult(
+                CompiledExecutionRunner.FailureResult(plan, executable, options, budget, error));
+        }
+        catch (SandboxRuntimeException ex)
+        {
+            return PreparedExecutionResult.FromResult(
+                CompiledExecutionRunner.FailureResult(plan, executable, options, budget, ex.Error));
+        }
+        catch (Exception)
+        {
+            var error = new SandboxError(SandboxErrorCode.HostFailure, "compiled sandbox execution failed");
+            return PreparedExecutionResult.FromResult(
+                CompiledExecutionRunner.FailureResult(plan, executable, options, budget, error));
+        }
+    }
+}

src/SafeIR.Hosting/Execution/Prepared/PreparedExecutionResult.cs
@@ -1,0 +1,33 @@
+namespace SafeIR.Hosting;
+
+using SafeIR;
+
+internal readonly record struct PreparedExecutionResult(
+    bool Succeeded,
+    SandboxValue? Value,
+    SandboxError? Error,
+    ExecutionMode ActualMode,
+    string? ArtifactHash,
+    SandboxExecutionResult? FullResult)
+{
+    public static PreparedExecutionResult FromNoAuditSuccess(
+        SandboxValue value,
+        ExecutionMode actualMode,
+        string? artifactHash)
+        => new(
+            Succeeded: true,
+            value,
+            Error: null,
+            actualMode,
+            artifactHash,
+            FullResult: null);
+
+    public static PreparedExecutionResult FromResult(SandboxExecutionResult result)
+        => new(
+            result.Succeeded,
+            result.Value,
+            result.Error,
+            result.ActualMode,
+            result.ArtifactHash,
+            result);
+}

src/SafeIR.Hosting/Execution/Prepared/SandboxHost.PreparedValue.cs
@@ -1,0 +1,127 @@
+namespace SafeIR.Hosting;
+
+using System.Diagnostics;
+using SafeIR;
+
+public sealed partial class SandboxHost
+{
+    internal ValueTask<PreparedExecutionResult> ExecutePreparedValueInProcessAsync(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        CancellationToken cancellationToken = default)
+    {
+        Debug.Assert(options.Isolation == SandboxIsolation.InProcess);
+        Debug.Assert(Enum.IsDefined(options.Mode));
+
+        ThrowIfDisposed();
+        if (TryGetRevokedCapability(plan, entrypoint, out var revoked))
+        {
+            return PublishedResult(CapabilityRevokedResult(plan, options, revoked));
+        }
+
+        if (options.RequireDeterministic && !plan.Policy.Deterministic)
+        {
+            return PublishedResult(DeterminismRequiredResult(plan, options));
+        }
+
+        return CanTryCompiledNoAuditValue(plan, entrypoint, options)
+            ? ExecuteCompiledPreparedValueAsync(plan, entrypoint, input, options, cancellationToken)
+            : ExecutePreparedResultAsValueAsync(plan, entrypoint, input, options, cancellationToken);
+    }
+
+    private async ValueTask<PreparedExecutionResult> ExecuteCompiledPreparedValueAsync(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var executable = await _compiled.GetAsync(plan, entrypoint, cancellationToken).ConfigureAwait(false);
+            if (!CompiledExecutionRunner.CanUseNoAuditSuccessPath(
+                    plan,
+                    entrypoint,
+                    executable.Artifact,
+                    options,
+                    out var allowedBindings))
+            {
+                var fullResult = await CompiledExecutionRunner.ExecuteAsync(
+                        executable,
+                        plan,
+                        entrypoint,
+                        input,
+                        options,
+                        cancellationToken)
+                    .ConfigureAwait(false);
+                return PreparedExecutionResult.FromResult(Publish(fullResult));
+            }
+
+            var result = CompiledNoAuditValueRunner.Execute(
+                executable,
+                plan,
+                entrypoint,
+                input,
+                options,
+                allowedBindings,
+                cancellationToken);
+            return result.FullResult is null
+                ? result
+                : PreparedExecutionResult.FromResult(Publish(result.FullResult));
+        }
+        catch (SandboxRuntimeException ex) when (CanFallback(options, ex))
+        {
+            var fallback = await ExecuteFallbackToInterpreterAsync(
+                    plan,
+                    entrypoint,
+                    input,
+                    options,
+                    ex.Error,
+                    cancellationToken)
+                .ConfigureAwait(false);
+            return PreparedExecutionResult.FromResult(Publish(fallback));
+        }
+        catch (SandboxRuntimeException ex)
+        {
+            return PreparedExecutionResult.FromResult(Publish(CompiledFailureResult(plan, options, ex.Error)));
+        }
+        catch (OperationCanceledException)
+        {
+            var error = new SandboxError(SandboxErrorCode.Cancelled, "execution cancelled");
+            return PreparedExecutionResult.FromResult(Publish(CompiledFailureResult(plan, options, error)));
+        }
+        catch (Exception)
+        {
+            var error = new SandboxError(SandboxErrorCode.HostFailure, "compiled execution failed");
+            return PreparedExecutionResult.FromResult(Publish(CompiledFailureResult(plan, options, error)));
+        }
+    }
+
+    private async ValueTask<PreparedExecutionResult> ExecutePreparedResultAsValueAsync(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        CancellationToken cancellationToken)
+    {
+        var result = await ExecutePreparedInProcessAsync(plan, entrypoint, input, options, cancellationToken)
+            .ConfigureAwait(false);
+        return PreparedExecutionResult.FromResult(result);
+    }
+
+    private bool CanTryCompiledNoAuditValue(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxExecutionOptions options)
+        => _compiled.IsAvailable &&
+           options.Mode == ExecutionMode.Compiled &&
+           !options.EnableDebugTrace &&
+           options.SuppressSuccessfulRunSummaryAudit &&
+           plan.BindingReferences.TryGetValue(entrypoint, out var allowedBindings) &&
+           allowedBindings.Count == 0;
+
+    private ValueTask<PreparedExecutionResult> PublishedResult(SandboxExecutionResult result)
+        => ValueTask.FromResult(PreparedExecutionResult.FromResult(Publish(result)));
+}

src/SafeIR.Plugins/Kernel/InstalledKernel.Execution.cs
@@ -1,0 +1,36 @@
+namespace SafeIR.Plugins;
+
+using SafeIR;
+using SafeIR.Hosting;
+
+public sealed partial class InstalledKernel
+{
+    private async ValueTask<SandboxValue> ExecutePreparedAsync(
+        string entrypoint,
+        SandboxValue input,
+        CancellationToken cancellationToken)
+    {
+        using var executionCancellation = PluginExecutionCancellation.Create(
+            cancellationToken,
+            _revocation.Token);
+        var result = await _host.ExecutePreparedValueInProcessAsync(
+                _plan,
+                entrypoint,
+                input,
+                _executionOptions,
+                executionCancellation.Token)
+            .ConfigureAwait(false);
+        _executionObserver.Record(entrypoint, _executionMode, result);
+        if (IsRevoked)
+        {
+            PluginKernelRevocation.ThrowIfRevoked(true);
+        }
+
+        if (!result.Succeeded)
+        {
+            throw new SandboxRuntimeException(result.Error ?? new SandboxError(SandboxErrorCode.HostFailure, "kernel execution failed"));
+        }
+
+        return result.Value ?? SandboxValue.Unit;
+    }
+}

src/SafeIR.Plugins/Runtime/PluginExecutionObservation.cs
@@ -1,6 +1,7 @@
 namespace SafeIR.Plugins;
 
 using SafeIR;
+using SafeIR.Hosting;
 
 public sealed record PluginExecutionObservation(
     string Entrypoint,
@@ -58,6 +59,12 @@ internal sealed class PluginExecutionObserver
 
     public void Record(string entrypoint, ExecutionMode requestedMode, SandboxExecutionResult result)
     {
+        if (result is { Succeeded: true, Error: null, AuditEvents.Count: 0 })
+        {
+            RecordNoAuditSuccess(entrypoint, requestedMode, result.ActualMode, result.ArtifactHash);
+            return;
+        }
+
         ExtractMarkers(requestedMode, result, out var summary, out var fallbackReason);
         var observation = new ObservationEntry(
             entrypoint,
@@ -71,7 +78,40 @@ internal sealed class PluginExecutionObserver
             Field(summary, "cacheKey"),
             result.ArtifactHash ?? Field(summary, "artifactHash"),
             Field(summary, "materializationStatus"));
+        Append(observation);
+    }
 
+    public void Record(string entrypoint, ExecutionMode requestedMode, PreparedExecutionResult result)
+    {
+        if (result.FullResult is { } fullResult)
+        {
+            Record(entrypoint, requestedMode, fullResult);
+            return;
+        }
+
+        RecordNoAuditSuccess(entrypoint, requestedMode, result.ActualMode, result.ArtifactHash);
+    }
+
+    private void RecordNoAuditSuccess(
+        string entrypoint,
+        ExecutionMode requestedMode,
+        ExecutionMode actualMode,
+        string? artifactHash)
+        => Append(new ObservationEntry(
+            entrypoint,
+            requestedMode,
+            actualMode,
+            Succeeded: true,
+            ErrorCode: null,
+            FallbackReason: null,
+            CacheStatus: "None",
+            RuntimeForm: null,
+            CacheKey: null,
+            artifactHash,
+            MaterializationStatus: null));
+
+    private void Append(ObservationEntry observation)
+    {
         lock (_gate)
         {
             var slot = (_start + _count) % HistoryCapacity;

JKamsker/DotBoxd

(cherry picked from commit 09aac7e19ad0d72abca8529197231f1637856466)
Ported-from: JKamsker/SafeIR@7eec69181fdea5dbce6e199cbd64fe89d61adbaf
Source-branch: topic/best-of/plugin-allocation-trims
Source-PR: https://github.com/JKamsker/SafeIR/pull/12

benchmarks/DotBoxD.Kernels.Benchmarks/Examples/PreparedValueProbe.cs
@@ -1,0 +1,70 @@
+namespace DotBoxD.Kernels.Benchmarks.Examples;
+
+using System.Diagnostics;
+using DotBoxD.Kernels;
+using DotBoxD.Plugins;
+
+internal static class PreparedValueProbe
+{
+    public static async Task RunAsync()
+    {
+        const int warmup = 2_000;
+        const int iterations = 200_000;
+        var e = new ExampleWorkflowProbe.DamageEvent("ice", 120, "player-1");
+        var summary = await MeasureShouldHandleMissAsync(warmup, iterations, e);
+
+        Console.WriteLine("case                         iterations   elapsed       allocated      handled");
+        Console.WriteLine(
+            $"compiled no-audit miss {iterations,14:N0} {summary.Milliseconds,8:N1} ms {summary.AllocatedBytes,13:N0} B {summary.Handled,10:N0}");
+    }
+
+    private static async Task<RunSummary> MeasureShouldHandleMissAsync(
+        int warmup,
+        int iterations,
+        ExampleWorkflowProbe.DamageEvent e)
+    {
+        using var server = PluginServer.Create(
+            new InMemoryPluginMessageSink(),
+            defaultPolicy: MessageWritePolicy(),
+            executionMode: ExecutionMode.Compiled);
+        var kernel = await server.InstallJsonAsync(ExampleWorkflowProbe.FireDamagePackageJson());
+        var adapter = ExampleWorkflowProbe.DamageEventAdapter.Instance;
+
+        for (var i = 0; i < warmup; i++)
+        {
+            _ = await kernel.ShouldHandleAsync(adapter, e);
+        }
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        var allocatedBefore = GC.GetAllocatedBytesForCurrentThread();
+        var sw = Stopwatch.StartNew();
+        var handled = 0;
+        for (var i = 0; i < iterations; i++)
+        {
+            if (await kernel.ShouldHandleAsync(adapter, e))
+            {
+                handled++;
+            }
+        }
+
+        sw.Stop();
+        var allocatedBytes = GC.GetAllocatedBytesForCurrentThread() - allocatedBefore;
+        return new RunSummary(sw.Elapsed.TotalMilliseconds, allocatedBytes, handled);
+    }
+
+    private static SandboxPolicy MessageWritePolicy()
+        => SandboxPolicyBuilder.Create()
+            .GrantLogging()
+            .GrantHostMessageWrite()
+            .WithFuel(long.MaxValue)
+            .WithMaxHostCalls(int.MaxValue)
+            .WithMaxLoopIterations(long.MaxValue)
+            .WithMaxTotalStringBytes(long.MaxValue)
+            .WithWallTime(TimeSpan.FromMinutes(5))
+            .Build();
+
+    private readonly record struct RunSummary(double Milliseconds, long AllocatedBytes, int Handled);
+}

src/Hosting/DotBoxD.Hosting/Execution/Prepared/CompiledNoAuditValueRunner.cs
@@ -1,0 +1,58 @@
+namespace DotBoxD.Hosting;
+
+using DotBoxD.Kernels;
+
+internal static class CompiledNoAuditValueRunner
+{
+    public static PreparedExecutionResult Execute(
+        CompiledExecutable executable,
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        IReadOnlySet<string> allowedBindings,
+        CancellationToken cancellationToken)
+    {
+        var artifact = executable.Artifact;
+        var budget = new ResourceMeter(plan.Budget);
+        var context = new SandboxContext(
+            SandboxRunId.Suppressed,
+            plan.Policy,
+            budget,
+            plan.Bindings,
+            NoopAuditSink.Instance,
+            cancellationToken,
+            allowedBindings,
+            plan.ModuleHash,
+            plan.PolicyHash);
+
+        try
+        {
+            budget.CheckDeadline();
+            context.ChargeValue(input);
+            var value = artifact.Entrypoint(context, input);
+            CompiledExecutionRunner.EnsureReturnType(plan, entrypoint, value);
+            return PreparedExecutionResult.FromNoAuditSuccess(
+                value,
+                ExecutionMode.Compiled,
+                artifact.ArtifactHash);
+        }
+        catch (OperationCanceledException)
+        {
+            var error = new SandboxError(SandboxErrorCode.Cancelled, "execution cancelled");
+            return PreparedExecutionResult.FromResult(
+                CompiledExecutionRunner.FailureResult(plan, executable, options, budget, error));
+        }
+        catch (SandboxRuntimeException ex)
+        {
+            return PreparedExecutionResult.FromResult(
+                CompiledExecutionRunner.FailureResult(plan, executable, options, budget, ex.Error));
+        }
+        catch (Exception)
+        {
+            var error = new SandboxError(SandboxErrorCode.HostFailure, "compiled sandbox execution failed");
+            return PreparedExecutionResult.FromResult(
+                CompiledExecutionRunner.FailureResult(plan, executable, options, budget, error));
+        }
+    }
+}

src/Hosting/DotBoxD.Hosting/Execution/Prepared/PreparedExecutionResult.cs
@@ -1,0 +1,33 @@
+namespace DotBoxD.Hosting;
+
+using DotBoxD.Kernels;
+
+internal readonly record struct PreparedExecutionResult(
+    bool Succeeded,
+    SandboxValue? Value,
+    SandboxError? Error,
+    ExecutionMode ActualMode,
+    string? ArtifactHash,
+    SandboxExecutionResult? FullResult)
+{
+    public static PreparedExecutionResult FromNoAuditSuccess(
+        SandboxValue value,
+        ExecutionMode actualMode,
+        string? artifactHash)
+        => new(
+            Succeeded: true,
+            value,
+            Error: null,
+            actualMode,
+            artifactHash,
+            FullResult: null);
+
+    public static PreparedExecutionResult FromResult(SandboxExecutionResult result)
+        => new(
+            result.Succeeded,
+            result.Value,
+            result.Error,
+            result.ActualMode,
+            result.ArtifactHash,
+            result);
+}

src/Hosting/DotBoxD.Hosting/Execution/Prepared/SandboxHost.PreparedValue.cs
@@ -1,0 +1,127 @@
+namespace DotBoxD.Hosting;
+
+using System.Diagnostics;
+using DotBoxD.Kernels;
+
+public sealed partial class SandboxHost
+{
+    internal ValueTask<PreparedExecutionResult> ExecutePreparedValueInProcessAsync(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        CancellationToken cancellationToken = default)
+    {
+        Debug.Assert(options.Isolation == SandboxIsolation.InProcess);
+        Debug.Assert(Enum.IsDefined(options.Mode));
+
+        ThrowIfDisposed();
+        if (TryGetRevokedCapability(plan, entrypoint, out var revoked))
+        {
+            return PublishedResult(CapabilityRevokedResult(plan, options, revoked));
+        }
+
+        if (options.RequireDeterministic && !plan.Policy.Deterministic)
+        {
+            return PublishedResult(DeterminismRequiredResult(plan, options));
+        }
+
+        return CanTryCompiledNoAuditValue(plan, entrypoint, options)
+            ? ExecuteCompiledPreparedValueAsync(plan, entrypoint, input, options, cancellationToken)
+            : ExecutePreparedResultAsValueAsync(plan, entrypoint, input, options, cancellationToken);
+    }
+
+    private async ValueTask<PreparedExecutionResult> ExecuteCompiledPreparedValueAsync(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var executable = await _compiled.GetAsync(plan, entrypoint, cancellationToken).ConfigureAwait(false);
+            if (!CompiledExecutionRunner.CanUseNoAuditSuccessPath(
+                    plan,
+                    entrypoint,
+                    executable.Artifact,
+                    options,
+                    out var allowedBindings))
+            {
+                var fullResult = await CompiledExecutionRunner.ExecuteAsync(
+                        executable,
+                        plan,
+                        entrypoint,
+                        input,
+                        options,
+                        cancellationToken)
+                    .ConfigureAwait(false);
+                return PreparedExecutionResult.FromResult(Publish(fullResult));
+            }
+
+            var result = CompiledNoAuditValueRunner.Execute(
+                executable,
+                plan,
+                entrypoint,
+                input,
+                options,
+                allowedBindings,
+                cancellationToken);
+            return result.FullResult is null
+                ? result
+                : PreparedExecutionResult.FromResult(Publish(result.FullResult));
+        }
+        catch (SandboxRuntimeException ex) when (CanFallback(options, ex))
+        {
+            var fallback = await ExecuteFallbackToInterpreterAsync(
+                    plan,
+                    entrypoint,
+                    input,
+                    options,
+                    ex.Error,
+                    cancellationToken)
+                .ConfigureAwait(false);
+            return PreparedExecutionResult.FromResult(Publish(fallback));
+        }
+        catch (SandboxRuntimeException ex)
+        {
+            return PreparedExecutionResult.FromResult(Publish(CompiledFailureResult(plan, options, ex.Error)));
+        }
+        catch (OperationCanceledException)
+        {
+            var error = new SandboxError(SandboxErrorCode.Cancelled, "execution cancelled");
+            return PreparedExecutionResult.FromResult(Publish(CompiledFailureResult(plan, options, error)));
+        }
+        catch (Exception)
+        {
+            var error = new SandboxError(SandboxErrorCode.HostFailure, "compiled execution failed");
+            return PreparedExecutionResult.FromResult(Publish(CompiledFailureResult(plan, options, error)));
+        }
+    }
+
+    private async ValueTask<PreparedExecutionResult> ExecutePreparedResultAsValueAsync(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxValue input,
+        SandboxExecutionOptions options,
+        CancellationToken cancellationToken)
+    {
+        var result = await ExecutePreparedInProcessAsync(plan, entrypoint, input, options, cancellationToken)
+            .ConfigureAwait(false);
+        return PreparedExecutionResult.FromResult(result);
+    }
+
+    private bool CanTryCompiledNoAuditValue(
+        ExecutionPlan plan,
+        string entrypoint,
+        SandboxExecutionOptions options)
+        => _compiled.IsAvailable &&
+           options.Mode == ExecutionMode.Compiled &&
+           !options.EnableDebugTrace &&
+           options.SuppressSuccessfulRunSummaryAudit &&
+           plan.BindingReferences.TryGetValue(entrypoint, out var allowedBindings) &&
+           allowedBindings.Count == 0;
+
+    private ValueTask<PreparedExecutionResult> PublishedResult(SandboxExecutionResult result)
+        => ValueTask.FromResult(PreparedExecutionResult.FromResult(Publish(result)));
+}

src/Hosting/DotBoxD.Plugins/Kernel/InstalledKernel.Execution.cs
@@ -1,0 +1,36 @@
+namespace DotBoxD.Plugins;
+
+using DotBoxD.Hosting;
+using DotBoxD.Kernels;
+
+public sealed partial class InstalledKernel
+{
+    private async ValueTask<SandboxValue> ExecutePreparedAsync(
+        string entrypoint,
+        SandboxValue input,
+        CancellationToken cancellationToken)
+    {
+        using var executionCancellation = PluginExecutionCancellation.Create(
+            cancellationToken,
+            _revocation.Token);
+        var result = await _host.ExecutePreparedValueInProcessAsync(
+                _plan,
+                entrypoint,
+                input,
+                _executionOptions,
+                executionCancellation.Token)
+            .ConfigureAwait(false);
+        _executionObserver.Record(entrypoint, _executionMode, result);
+        if (IsRevoked)
+        {
+            PluginKernelRevocation.ThrowIfRevoked(true);
+        }
+
+        if (!result.Succeeded)
+        {
+            throw new SandboxRuntimeException(result.Error ?? new SandboxError(SandboxErrorCode.HostFailure, "kernel execution failed"));
+        }
+
+        return result.Value ?? SandboxValue.Unit;
+    }
+}

src/Hosting/DotBoxD.Plugins/Runtime/PluginExecutionObservation.cs
@@ -1,5 +1,6 @@
 namespace DotBoxD.Plugins;
 
+using DotBoxD.Hosting;
 using DotBoxD.Kernels;
 
 public sealed record PluginExecutionObservation(
@@ -58,6 +59,12 @@ internal sealed class PluginExecutionObserver
 
     public void Record(string entrypoint, ExecutionMode requestedMode, SandboxExecutionResult result)
     {
+        if (result is { Succeeded: true, Error: null, AuditEvents.Count: 0 })
+        {
+            RecordNoAuditSuccess(entrypoint, requestedMode, result.ActualMode, result.ArtifactHash);
+            return;
+        }
+
         ExtractMarkers(requestedMode, result, out var summary, out var fallbackReason);
         var observation = new ObservationEntry(
             entrypoint,
@@ -71,7 +78,40 @@ internal sealed class PluginExecutionObserver
             Field(summary, "cacheKey"),
             result.ArtifactHash ?? Field(summary, "artifactHash"),
             Field(summary, "materializationStatus"));
+        Append(observation);
+    }
 
+    public void Record(string entrypoint, ExecutionMode requestedMode, PreparedExecutionResult result)
+    {
+        if (result.FullResult is { } fullResult)
+        {
+            Record(entrypoint, requestedMode, fullResult);
+            return;
+        }
+
+        RecordNoAuditSuccess(entrypoint, requestedMode, result.ActualMode, result.ArtifactHash);
+    }
+
+    private void RecordNoAuditSuccess(
+        string entrypoint,
+        ExecutionMode requestedMode,
+        ExecutionMode actualMode,
+        string? artifactHash)
+        => Append(new ObservationEntry(
+            entrypoint,
+            requestedMode,
+            actualMode,
+            Succeeded: true,
+            ErrorCode: null,
+            FallbackReason: null,
+            CacheStatus: "None",
+            RuntimeForm: null,
+            CacheKey: null,
+            artifactHash,
+            MaterializationStatus: null));
+
+    private void Append(ObservationEntry observation)
+    {
         lock (_gate)
         {
             var slot = (_start + _count) % HistoryCapacity;